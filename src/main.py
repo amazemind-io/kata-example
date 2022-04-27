8b620b4cd47874afccd42dcf9fbfea42760736fa
Nikita Zavyalov <nikita.zavyalov@trivago.com>
2022-04-27 10:34:58 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,3 +5,6 @@ from email import message
 def welcomeUser(user):
     message = "" # use string formating to include the user variable in the message
     return message
+
+
+print(welcomeUser("Rob")) # Example test. Output should be "Welcome, Rob". Feel free to add more tests here for practice. 
